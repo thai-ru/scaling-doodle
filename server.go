28cdad2d05f014461418ef0572dac974f5445bf6
thai-ru <robinthairu34@gmail.com>
2023-10-07 05:45:34 +0300

    initial project setup, Handle GET and POST requests

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"gin/controller"
+	"gin/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.DELETE("/videos/", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
